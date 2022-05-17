dcb40ce8b02db52b79627ee619f53bfdd8b517a3
Eric Quelch <74473426+quelchx@users.noreply.github.com>
2022-05-17 18:10:49 -0400

    Update _app.tsx

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -2,26 +2,38 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { AuthProvider } from "../context/auth";
 import { ChakraProvider } from "@chakra-ui/react";
+import { useRouter } from "next/router";
 
 import Navbar from "../components/navbar/navbar";
 import Head from "../components/head/head";
 import Sidebar from "../components/sidebar/sidebar";
 import Axios from "axios";
 
-
 Axios.defaults.baseURL = process.env.NEXT_PUBLIC_SERVER_BASE_URL;
 Axios.defaults.withCredentials = true;
 
 export default function App({ Component, pageProps }: AppProps) {
+  const { pathname } = useRouter();
+  const authRoutes = ["/register", "/login"];
+  const isAuthRoute = authRoutes.includes(pathname);
   return (
     <ChakraProvider>
       <AuthProvider>
         <Head />
-        <Navbar />
-        <Sidebar>
-          {/* @ts-ignore */}
-          <Component {...pageProps} />
-        </Sidebar>
+        {!isAuthRoute ? (
+          <>
+            <Navbar />
+            <Sidebar>
+              {/* @ts-ignore */}
+              <Component {...pageProps} />
+            </Sidebar>
+          </>
+        ) : (
+          <>
+            {/* @ts-ignore */}
+            <Component {...pageProps} />
+          </>
+        )}
       </AuthProvider>
     </ChakraProvider>
   );
