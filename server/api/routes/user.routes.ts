99025f0b138b70a5888e8eea203aa5454733bd54
Eric Quelch <74473426+quelchx@users.noreply.github.com>
2022-05-17 18:11:25 -0400

    Update user.routes.ts

--- server/api/routes/user.routes.ts
+++ server/api/routes/user.routes.ts
@@ -14,6 +14,6 @@ router.put("/:id", user, auth, updateUser);
 router.delete("/:id", user, auth, deleteUser);
 
 router.get("/", getAllUsers);
-router.get("/:id", getUserById);
+router.get("/user/:id", getUserById);
 
 export default router;
