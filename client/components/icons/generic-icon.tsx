f89822113eaa1139b84240d32d0d2e47cb798756
Eric Quelch <74473426+quelchx@users.noreply.github.com>
2022-05-18 13:46:56 -0400

    Create generic-icon.tsx

---
+++ client/components/icons/generic-icon.tsx
@@ -0,0 +1,20 @@
+import { Box, chakra, HStack } from "@chakra-ui/react";
+import { IconComponent } from "../../@types";
+
+const GenericIcon = ({ icon, text }: IconComponent) => {
+  const Icon = () => icon;
+  return (
+    <HStack gap={1} justify="flex-start" alignItems={"center"} width="100%">
+      <Box pt={0.5}>
+        <Icon />
+      </Box>
+      <Box>
+        <chakra.p fontFamily={"monospace"} fontSize={14} pt={0.5}>
+          {text}
+        </chakra.p>
+      </Box>
+    </HStack>
+  );
+};
+
+export default GenericIcon;
